4db5a3974a51e7d71508247eaad6abfd342bccc8
ecrubio <ecrubio@miners.utep.edu>
2020-11-04 22:54:29 -0700

    Queue structure

--- threadDisplay.py
+++ threadDisplay.py
@@ -2,12 +2,35 @@ from threading import Thread, Semaphore
 import cv2
 import numpy as np
 
-#global 
+#global
 clipName = 'clip.mp4'
-frameQueue = []
-grayScaleQueue = []
+frameQueue = impQueue()
+grayScaleQueue = impQueue()
 
-def Extract():        
+class impQueue:
+
+    def __init__(self):
+        self.queue = []
+        self.capacity = threading.Semaphore(10) #max 10 frames
+        self.remaining = threading.Semaphore(0) #used
+        self.lock = threading.Lock()
+
+    def enqueue(self, frame):
+        self.capacity.acquire()
+        self.lock.acquire()
+        self.queue.append(item)
+        self.lock.release()
+        self.remaining.release()
+
+    def dequeue(self):
+        self.remaining.acquire()
+        self.lock.acquire()
+        self.queue.pop(item)
+        self.lock.release()
+        self.capacity.release()
+        return frame
+
+def Extract(Thread):
     return
 
 def GrayscaleConversion():
